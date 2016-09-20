b781d69ae8d6223e639edf8a69c063a25c28dfc2
Roma Ermochenkov <shnyrok.info@gmail.com>
2016-09-21 00:38:25 +0300

    Salaries sum

--- LearnJavaScript.ru/Object/SalariesSum.js
+++ LearnJavaScript.ru/Object/SalariesSum.js
@@ -1,25 +1,12 @@
-//Есть объект salaries с зарплатами. Напишите код, который выведет сумму всех зарплат.
+/*Сумма свойств
+важность: 5решение
+Есть объект salaries с зарплатами. Напишите код, который выведет сумму всех зарплат.
 
-//Если объект пустой, то результат должен быть 0.
+Если объект пустой, то результат должен быть 0.
 
-//Например:
+Например:
 
-//"use strict";
-
-//var salaries = {
-  //"Вася": 100,
-  //"Петя": 300,
-  //"Даша": 250
-//};
-
-function sumSalaries(obj){
-  var sum = 0;
-  for (key in obj){
-   sum = sum + obj[key]
-  }
-
-	return(sum)
-}
+"use strict";
 
 var salaries = {
   "Вася": 100,
@@ -27,4 +14,28 @@ var salaries = {
   "Даша": 250
 };
 
-sumSalaries(salaries)
+//... ваш код выведет 650
+P.S. Сверху стоит use strict, чтобы не забыть объявить переменные.
+*/
+
+var salaries = {
+  "Вася": 100,
+  "Петя": 300,
+  "Даша": 250
+};
+
+
+var fal = {}
+function sum(obj){
+  var salaries = 0;
+  for (var key in obj) {
+    if (!obj[key]) {
+      return 0;
+    }
+    salaries += obj[key]
+  }
+  return salaries
+}
+
+console.log(sum(salaries))
+console.log(sum(fal))
