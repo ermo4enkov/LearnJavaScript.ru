104ceea08cf03083b828e9d375ea4d14ef144fba
Roma Ermochenkov <ermo4enkov@gmail.com>
2016-09-22 22:58:04 +0300

    Added find element in array

---
+++ LearnJavaScript.ru/Array/findEl.js
@@ -0,0 +1,33 @@
+/*Создайте функцию find(arr, value), которая ищет в массиве arr 
+значение value и возвращает его номер, 
+если найдено, или -1, если не найдено.
+
+Например:
+
+arr = ["test", 2, 1.5, false];
+
+find(arr, "test"); // 0
+find(arr, 2); // 1
+find(arr, 1.5); // 2
+
+find(arr, 0); // -1*/
+
+
+arr = ["test", 2, 1.5, false];
+
+function find(arr, el){
+  
+  for (var i = 0; i < arr.length; i++) {
+    
+    if (arr[i] === el) {
+      return i;
+    }
+    
+  }
+  return -1;
+}
+
+console.log(find(arr, "test"))
+console.log(find(arr, 2)); // 1
+console.log(find(arr, 1.5)); // 2
+console.log(find(arr, 0)); // -1
