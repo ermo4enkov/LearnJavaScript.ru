4d1100643fd7b0cb22efce5a888f291606cde2c0
Roma Ermochenkov <ermo4enkov@gmail.com>
2016-09-27 00:52:18 +0300

    Added array map

---
+++ LearnJavaScript.ru/Array/arrayMap.js
@@ -0,0 +1,25 @@
+// Code goes here
+
+/*Перепишите цикл через map
+
+Код ниже получает из массива строк новый массив, содержащий их длины:
+
+ 
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+
+var arrLength = [];
+for (var i = 0; i < arr.length; i++) {
+  arrLength[i] = arr[i].length;
+}
+
+alert( arrLength ); // 4,5,2,5
+Перепишите выделенный участок: уберите цикл, 
+используйте вместо него метод map.*/
+
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+
+var arrLength = arr.map(function (i){
+  return i.length
+})
+
+console.log(arrLength)
