d8c7b5049ac76e7d4f9c169de96e1a391414b573
Roma Ermochenkov <ermo4enkov@gmail.com>
2016-10-26 00:33:37 +0300

    Added constant

--- LearnJavaScript.ru/Descriptors/descriptor.js
+++ LearnJavaScript.ru/Descriptors/descriptor.js
@@ -1,5 +1,26 @@
-var user = {}
+//Ниже мы создали св-во объекта User, в котором указали,
+//что свойство может быть изменяемым, настраиваемым и перечислимым
+var user = {};
 
-Object.defineProperty(user, "name", {value: "Vasya", configurable: true, writable: true, enumerable: true});
+Object.defineProperty(user, "name", {
+											value: "Vasya", 
+											configurable: true, 
+											writable: true, 
+											enumerable: true});
 
 console.log(user.name);
+
+//Здесь же мы создали св-во объекта user2, в котором указали,
+//что свойство может быть константой
+
+var user2 = {};
+
+Object.defineProperty(user2, "name", {
+											value: "Roma",
+											configurable: false,
+											writable: false,
+											enumerable: false});
+
+user2.name = "Petya"
+
+console.log(user2.name)
