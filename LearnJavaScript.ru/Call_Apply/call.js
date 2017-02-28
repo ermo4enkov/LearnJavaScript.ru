e07567b9c4b8332694b3a4c37664b5ae093f6077
ermo4enkov <ermo4enkov@gmail.com>
2017-02-28 20:38:42 +0200

    Call continue

--- LearnJavaScript.ru/Call_Apply/call.js
+++ LearnJavaScript.ru/Call_Apply/call.js
@@ -17,3 +17,31 @@ showName.call(user);
 
 
 // Метод .call позволяет явно передать контекст функции
+
+
+function showPartName(firstPart, secondPart) {
+    console.log(this[firstPart] + " " + this[secondPart])
+}
+
+var randomUser = {
+    name: "Roma",
+    secondName: "Ermo",
+    age: 23
+};
+
+showPartName.call(randomUser, "name", "age");
+
+
+// Borrow methods
+
+function printArgs() {
+
+    var join = [].join;
+    //в переменную мы записали заимствованный метод от массива
+    var args = join.call(arguments, ":");
+    // а потом вызвали его с помощью метода call
+
+    console.log(args)
+}
+
+printArgs(1,2,3,4,5,"sooqa");
