3daa6b36049cae743fa415730d79da7c6363dee3
ermo4enkov <ermo4enkov@gmail.com>
2017-03-06 01:00:13 +0200

    Added stringify

--- LearnJavaScript.ru/JSON/parse.js
+++ LearnJavaScript.ru/JSON/parse.js
@@ -6,3 +6,13 @@
 var arr = ["1", "2", 3, 5];
 
 console.log(JSON.parse(arr[2])) // 3
+
+
+var str = '{"title":"Конференция","date":"2014-11-30T12:00:00.000Z"}';
+
+var event = JSON.parse(str, function (key, value) {
+    if (key == 'date') return new Date(value);
+    return value;
+});
+
+console.log(event.date.getDate());
---
+++ LearnJavaScript.ru/JSON/stringify.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ermo4enkov on 3/6/17.
+ */
+
+// Allow transform usuall object to json
+
+var event = {
+    title: "confa",
+    date: "today"
+};
+
+var Jstr = JSON.stringify(event);
+
+console.log(Jstr)
