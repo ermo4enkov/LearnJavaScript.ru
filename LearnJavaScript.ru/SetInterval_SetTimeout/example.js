713ab66e172b73e6014dc4ed15d5410da4117571
ermo4enkov <ermo4enkov@gmail.com>
2017-03-07 20:08:57 +0200

    Added recursion setTimeOut

--- LearnJavaScript.ru/SetInterval_SetTimeout/example.js
+++ LearnJavaScript.ru/SetInterval_SetTimeout/example.js
@@ -1,3 +1,9 @@
 /**
  * Created by ermo4enkov on 3/7/17.
  */
+//Recursuve setTimeout
+
+var timerId = setTimeout(function tick(){
+    console.log("Tick");
+    timerId = setTimeout(tick, 2000);
+}, 2000);
