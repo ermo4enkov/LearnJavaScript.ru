508232bb7a6b948276e74f45eec58ba8eb32e278
ermo4enkov <ermo4enkov@gmail.com>
2017-03-07 20:39:35 +0200

    Added first example

---
+++ LearnJavaScript.ru/TryCatch/example.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ermo4enkov on 3/7/17.
+ */
+
+
+try {
+    console.log("All cool");
+    llalla;
+} catch (ex) {
+    console.log(ex);
+}
+
+// в ex передастся причина выброса ошибки
