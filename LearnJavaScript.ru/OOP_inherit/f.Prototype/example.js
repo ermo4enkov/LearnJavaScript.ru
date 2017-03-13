6745f24b9c17db90e729349fde6930c4aa39d271
ermo4enkov <ermo4enkov@gmail.com>
2017-03-13 23:20:11 +0200

    Created first example

--- LearnJavaScript.ru/OOP_inherit/f.Prototype/example.js
+++ LearnJavaScript.ru/OOP_inherit/f.Prototype/example.js
@@ -1,3 +1,26 @@
 /**
  * Created by ermo4enkov on 3/13/17.
  */
+
+// Как указать __proto__ через функцию конструктор
+
+// 1. Укажем в свойстве конструктора ссылк на конкретный объект
+// Это не работает в IE 10-
+
+var animal = {
+    name: "Cow",
+    age: "33",
+    myNaMEaNDaGE: function(){
+        return ("name:" + this.name + ", age: " + this.age );
+    }
+};
+
+
+function Bull() {
+    this.color = 'Brown';
+    this.__proto__ = animal;
+}
+
+var bull = new Bull();
+
+console.log(bull.myNaMEaNDaGE());
