8140690cde5cc5b2a26815b335c72dcc3c3dbbe4
ermo4enkov <ermo4enkov@gmail.com>
2017-03-14 23:24:06 +0200

    Added example with own proto

--- LearnJavaScript.ru/OOP_inherit/Classes_On_Prototype/example.js
+++ LearnJavaScript.ru/OOP_inherit/Classes_On_Prototype/example.js
@@ -19,3 +19,20 @@ var leo = new Animal("Leo",23,"red");
 
 console.log(leo.run(233))
 
+
+
+// Other example with prototype
+
+function Beast(name){
+    this.name = name;
+    this.speed = 0;
+}
+
+Beast.prototype.run = function (speed) {
+    this.speed += speed;
+    console.log(this.name + "run with " + this.speed);
+}
+
+var bear = new Beast("pooh", 23);
+
+bear.run(44)
