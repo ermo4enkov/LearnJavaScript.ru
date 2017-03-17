b0c99a986b5759c763686a45ece865350d581675
ermo4enkov <ermo4enkov@gmail.com>
2017-03-17 21:08:44 +0200

    Added example to the object

---
+++ LearnJavaScript.ru/ES6/Object/example.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ermo4enkov on 3/17/17.
+ */
+
+
+// We can insert value to straight to the Object
+
+let name = 'Roma';
+let secondName = 'Ermo'
+
+let user = {
+    name: name,
+    secondName: secondName
+}
+
+console.log(user);
