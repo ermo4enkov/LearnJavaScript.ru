775f55e9632a3603eebbba17ee0e74127553e8e2
ermo4enkov <ermo4enkov@gmail.com>
2017-03-17 15:18:06 +0200

    finished destructure

--- LearnJavaScript.ru/ES6/Destruct/example.js
+++ LearnJavaScript.ru/ES6/Destruct/example.js
@@ -20,3 +20,39 @@ let [fff, ffff, ...rest] = "Юлий Цезарь Император Рима".s
 
 console.log(fff)
 console.log(rest)
+
+
+//по-умолчанию значения
+
+let [user="vasy", user2="vasya2"] = [];
+console.log(user);
+
+
+// В качестве значений по умолчанию можно указать не только примитивы но и функции
+
+function consoleName() {
+    return Date.now() + '-name'
+}
+
+let [c,c2=consoleName()] = ['Pupa']
+
+console.log(c);
+console.log(c2);
+
+// Object destruct
+
+
+// let {var1, var2} = {var1:…, var2…}
+
+
+let options = {
+    title: "Меню",
+    width: 100,
+    height: 200
+};
+
+let {title: one, width: two, height: three} = options;
+
+console.log(one)
+console.log(two)
+console.log(three)
