81cdd8edd36d0a3d051aa5b78e270f9bfe22ee5d
ermo4enkov <ermo4enkov@gmail.com>
2017-03-17 15:02:12 +0200

    Started destruct

--- LearnJavaScript.ru/ES6/Destruct/example.js
+++ LearnJavaScript.ru/ES6/Destruct/example.js
@@ -8,3 +8,15 @@ let [firstName, lastName] = ["Илья", "Кантор"];
 console.log(firstName); // Илья
 console.log(lastName);  // Кантор
 
+
+let [, , title] = "Юлий Цезарь Император Рима".split(" ");
+
+console.log(title);
+
+
+// spread operator
+
+let [fff, ffff, ...rest] = "Юлий Цезарь Император Рима".split(" ");
+
+console.log(fff)
+console.log(rest)
