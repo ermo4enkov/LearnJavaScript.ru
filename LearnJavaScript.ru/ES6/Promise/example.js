312e70b937ab5e086bfba9c31c540e16d83de003
ermo4enkov <ermo4enkov@gmail.com>
2017-03-22 23:09:35 +0200

    Created first example

--- LearnJavaScript.ru/ES6/Promise/example.js
+++ LearnJavaScript.ru/ES6/Promise/example.js
@@ -14,8 +14,39 @@
     // reject(ошибка) при ошибке
 
 
+// Универсальный метод для навешивания обработчиков:
+//
+//     promise.then(onFulfilled, onRejected)
+//
+// onFulfilled – функция, которая будет вызвана с результатом при resolve.
+//     onRejected – функция, которая будет вызвана с ошибкой при reject.
+
+
+
+// Принудительный выброс ошибки будет обработан синхронно
 let p = new Promise((resolve,reject) => {
     throw new Error(`fuck6 it's a trap`);
 });
 
-p.catch(console.log())
+p.catch(console.log());
+
+
+// Example with setTimeout
+
+let time = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(`result`);
+    }, 1000);
+})
+
+time
+    .then(
+        result => {
+            console.log("Fullfiled " + result)
+        },
+        error => {
+            console.log("Rejected" + error);
+        }
+    );
+
+// После выполнения промис уже ни может быть не изменён, ни дополнен
