0ac4b2f91dda0aa6752a36f0640c85328a9720c4
ermo4enkov <ermo4enkov@gmail.com>
2017-03-22 23:01:44 +0200

    Lte's go

--- LearnJavaScript.ru/ES6/Promise/example.js
+++ LearnJavaScript.ru/ES6/Promise/example.js
@@ -1,3 +1,21 @@
 /**
  * Created by ermo4enkov on 3/22/17.
  */
+
+
+// Promise syntax
+
+// let promise = new Promise(resolve, reject){}
+    // Эта функция будет вызвана автоматически
+
+    // В ней можно делать любые асинхронные операции,
+    // А когда они завершатся — нужно вызвать одно из:
+    // resolve(результат) при успешном выполнении
+    // reject(ошибка) при ошибке
+
+
+let p = new Promise((resolve,reject) => {
+    throw new Error(`fuck6 it's a trap`);
+});
+
+p.catch(console.log())
