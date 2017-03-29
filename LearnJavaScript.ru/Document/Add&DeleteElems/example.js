7c2e7a16e3a27fc63579f28c04a019c71753a860
ermo4enkov <ermo4enkov@gmail.com>
2017-03-29 16:02:58 +0300

    Created lesson Add&DeleteElems

---
+++ LearnJavaScript.ru/Document/Add&DeleteElems/example.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ermo4enkov on 3/29/17.
+ */
+
+//Создание элемента
+
+// document.createElement(elem)
+
+var div = document.createElement("div");
+
+div.className("cool");
+div.innerHTML("Some text");
