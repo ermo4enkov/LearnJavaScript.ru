5e597df98994714336c9ea9286f3237ffae67ebe
ermo4enkov <ermo4enkov@gmail.com>
2017-04-08 23:59:10 +0300

    Started new lesson

---
+++ LearnJavaScript.ru/Events/Introduction/example.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ermo4enkov on 4/8/17.
+ */
+
+// Назначение обработчиков событий
+
+// 1. Прямо в html-документе
+
+// <input value="one" type="submit" onclick="alert('click')">
+
+// 2. Использование свойства DOM-объекта
+
+// <input id="elem" type="button" value="Нажми меня" />
+//  <script>
+//      elem.onclick = function (){
+//                      alert("click!")
+//                   }
+// </script>
