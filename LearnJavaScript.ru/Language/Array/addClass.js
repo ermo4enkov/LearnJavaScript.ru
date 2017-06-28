3382f39df95fc5a29fba9db2d15b7f12fe02d5c4
ermo4enkov <ermo4enkov@gmail.com>
2017-06-28 17:59:24 +0300

    Added new way of solution

--- LearnJavaScript.ru/Language/Array/addClass.js
+++ LearnJavaScript.ru/Language/Array/addClass.js
@@ -1,37 +1,48 @@
-// Code goes here
+// Создайте функцию addClass(obj, cls), которая добавляет в список класс cls, но только если его там еще нет:
 
-/*В объекте есть свойство className, 
-которое содержит список «классов» – слов, разделенных пробелом:
+// addClass(obj, 'new'); // obj.className='open menu new'
+// addClass(obj, 'open'); // без изменений (класс уже существует)
+// addClass(obj, 'me'); // obj.className='open menu new me'
 
-
-Создайте функцию addClass(obj, cls), 
-которая добавляет в список класс cls, но только если его там еще нет:
-
-addClass(obj, 'new'); // obj.className='open menu new'
-addClass(obj, 'open'); // без изменений (класс уже существует)
-addClass(obj, 'me'); // obj.className='open menu new me'
-
-alert( obj.className ); // "open menu new me"*/
+// console.log( addClass(obj.className) ); // "open menu new me"
 
 var obj = {
-  className: 'open menu'
+    className: 'open menu'
 }
 
-function addClass (obj, cls) {
-  
-  var classes = obj.className ? obj.className.split(" "): []
-  
-  for (var i = 0; i < classes.length; i++) {
-    if (classes[i] != cls) {
-      classes.push(cls)
-    } break;
-  }
-  
-  obj.className = classes.join(" ") 
-  return obj;
-  
+
+// неправильное решение
+function addClass(obj, cls){
+    for(var item in obj){
+        obj[item] = obj[item].split(" ");
+        if (obj[item].indexOf(cls) == -1 ){
+            obj[item].push(cls)
+        }
+        return obj[item].join(", ");
+    }
 }
 
-console.log(addClass(obj, "new"))
-console.log(addClass(obj, 'open'))
-console.log(addClass(obj, 'me'))
+// console.log(addClass(obj, "22"))
+console.log(addClass(obj, "open"))
+
+
+//правильное решение
+
+function addClass_right(obj, cls){
+    var classes = obj.className ? obj.className.split(" "): [];
+
+    for (var i = 0; i < classes.length; i++){
+        if (classes[i] != cls){
+            classes.push(cls)
+            return obj.className = classes.join(" ")
+        }
+        return obj.className;
+    }
+
+}
+
+console.log(addClass_right(obj, "22"))
+console.log(addClass_right(obj, "open"))
+
+// console.log(addClass(obj, "22"))
+console.log(addClass(obj, "open"))
