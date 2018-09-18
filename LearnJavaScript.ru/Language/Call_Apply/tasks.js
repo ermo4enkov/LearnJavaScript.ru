8653b6c4c091ffdc67b9cc75fac4cc26040c6fcc
ermojob <ermojob@gmail.com>
2018-09-18 14:37:59 +0300

    #master modified tasks

--- LearnJavaScript.ru/Language/Call_Apply/tasks.js
+++ LearnJavaScript.ru/Language/Call_Apply/tasks.js
@@ -3,34 +3,30 @@
  */
 
 // Есть функция sum, которая суммирует все элементы массива:
-
-    // function sum(arr) {
-    //     return arr.reduce(function(a, b) {
-            // return a + b;
-        // });
-    // }
+// function sum(arr) {
+//     return arr.reduce(function(a, b) {
+// return a + b;
+// });
+// }
 
 // alert( sum([1, 2, 3]) ); // 6 (=1+2+3)
 
 // Создайте аналогичную функцию sumArgs(), которая будет суммировать все свои аргументы:
-
-    // function sumArgs() {
-        /* ваш код */
-    // }
+// function sumArgs() {
+/* ваш код */
+// }
 
 // alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через запятую, без массива
 
 // Для решения примените метод reduce к arguments, используя call, apply или одалживание метода.
-
-    // P.S. Функция sum вам не понадобится, она приведена в качестве примера использования reduce для похожей задачи.
-
+// P.S. Функция sum вам не понадобится, она приведена в качестве примера использования reduce для похожей задачи.
 
 function sumArgs(){
-    arguments.reduce = [].reduce;
-    return arguments.reduce(function (a,b) {
-        return a + b;
+    let args = [].slice.call(arguments);
+    return args.reduce((item, index) => {
+      return item + index;
     })
-}
-
-console.log(sumArgs(2,3,4));
-
+  }
+  
+console.log(sumArgs(2,3,3,4))
+  
